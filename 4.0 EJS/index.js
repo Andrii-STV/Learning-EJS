8c527cba538cd723e3b7ef5cae3d9e1e5cdc7e45
Andrii <stavnichenkoa2000@gmail.com>
2025-09-14 15:08:49 +0300

    Just  learned the very basics of EJS. Established a server with Express, wrote the logic which fetches the current day from the Date - current or manually inputed within the script. Then if it's a Sunday or Saturday, the learnDay() function returns one of two possible responses. It also names the day, which is fetched from the array of weekdays. It's handy, because getDay() function returns the numbers frrom 0 to 6, where 0 is Sunday, and 6 is Saturday. So these numbers can be used as array keys. Finally I get the home url and render ejs file. I add the object with the key "weekday" and the learnDay() function as a value, which returns a predefiend response. In the index.ejs I call weekday key inside <h1></h1> tag like that: <%= weekday %> and that's it! It works perfecctly fine! I'm so happy I wrote it myself without checking the tutorial even once :)

--- 4.0 EJS/index.js
+++ 4.0 EJS/index.js
@@ -1,1 +1,36 @@
-console.log("hello EJS");
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const todayDate = new Date("2025-09-15");
+const currentDay = todayDate.getDay();
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var whatToDoToday = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function learnDay() {
+    if ( currentDay === 0 || currentDay === 6) {
+        whatToDoToday = `It's ${dayNames[currentDay]}, - a weekend, lets party hard!`;
+    } else {
+        whatToDoToday = `It's ${dayNames[currentDay]}, - a week day, lets work and earn!`;
+    }
+    return whatToDoToday;
+}
+
+app.get("/", (req, res) => {
+    res.render((__dirname + "/views/index.ejs"), 
+    { weekday: learnDay() }
+    );
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Server is running on port ${port}`);
+}); 
